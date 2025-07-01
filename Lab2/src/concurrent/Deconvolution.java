0049726886843b6b5d978fe64b750f789e8e7889
Ana Rita Medeiros <100716949+Anaritamed@users.noreply.github.com>
2025-07-01 20:17:27 -0300

    chore: remove código duplicado

--- Lab2/src/concurrent/Deconvolution.java
+++ Lab2/src/concurrent/Deconvolution.java
@@ -60,63 +60,6 @@ public class Deconvolution {
         System.out.println("Imagem restaurada salva como restaurada.png");
     }
 
-    // Richardson-Lucy Iterativo
-    public static float[][] richardsonLucy(float[][] image, float[][] psf, float[][] psfFlipped, int iterations) {
-        int h = image.length;
-        int w = image[0].length;
-        float[][] estimate = new float[h][w];
-
-        // Inicializa com valor constante (pode ser a imagem borrada)
-        for (int y = 0; y < h; y++)
-            for (int x = 0; x < w; x++)
-                estimate[y][x] = 0.5f;
-
-        for (int it = 0; it < iterations; it++) {
-            float[][] estimateBlurred = convolve(estimate, psf);
-            float[][] ratio = new float[h][w];
-
-            for (int y = 0; y < h; y++)
-                for (int x = 0; x < w; x++) {
-                    float eb = estimateBlurred[y][x];
-                    ratio[y][x] = (eb > 1e-6f) ? image[y][x] / eb : 0f;
-                }
-
-            float[][] correction = convolve(ratio, psfFlipped);
-
-            for (int y = 0; y < h; y++)
-                for (int x = 0; x < w; x++)
-                    estimate[y][x] *= correction[y][x];
-        }
-
-        return estimate;
-    }
-
-    // Convolução 2D
-    public static float[][] convolve(float[][] image, float[][] kernel) {
-        int h = image.length, w = image[0].length;
-        int kh = kernel.length, kw = kernel[0].length;
-        int kyc = kh / 2, kxc = kw / 2;
-
-        float[][] result = new float[h][w];
-
-        for (int y = 0; y < h; y++) {
-            for (int x = 0; x < w; x++) {
-                float sum = 0f;
-                for (int ky = 0; ky < kh; ky++) {
-                    for (int kx = 0; kx < kw; kx++) {
-                        int iy = y + ky - kyc;
-                        int ix = x + kx - kxc;
-                        if (iy >= 0 && iy < h && ix >= 0 && ix < w) {
-                            sum += image[iy][ix] * kernel[ky][kx];
-                        }
-                    }
-                }
-                result[y][x] = sum;
-            }
-        }
-        return result;
-    }
-
     // Espelha o kernel horizontal e verticalmente
     public static float[][] invertKernel(float[][] kernel) {
         int h = kernel.length, w = kernel[0].length;
